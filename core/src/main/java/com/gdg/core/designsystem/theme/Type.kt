aed7f1a2c0db0213d9c273f39c7059d86b82ae3e
gaeulzzang <gaeulzzang@gmail.com>
2025-01-28 20:23:05 +0900

    #1 [SETTING] 폰트 다운로드

--- core/src/main/java/com/gdg/core/designsystem/theme/Type.kt
+++ core/src/main/java/com/gdg/core/designsystem/theme/Type.kt
@@ -1,10 +1,14 @@
 package com.gdg.core.designsystem.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.runtime.Stable
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.gdg.core.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -31,4 +35,18 @@ val Typography = Typography(
         letterSpacing = 0.5.sp
     )
     */
+)
+
+val PretendardBold = FontFamily(Font(R.font.pretendard_bold, FontWeight.Bold, FontStyle.Normal))
+val PretendardSemiBold =
+    FontFamily(Font(R.font.pretendard_semibold, FontWeight.SemiBold, FontStyle.Normal))
+val PretendardMedium =
+    FontFamily(Font(R.font.pretendard_medium, FontWeight.Medium, FontStyle.Normal))
+val PretendardRegular =
+    FontFamily(Font(R.font.pretendard_regular, FontWeight.Normal, FontStyle.Normal))
+val JalnanGothic = FontFamily(Font(R.font.jalnan_gothic, FontWeight.SemiBold, FontStyle.Normal))
+
+@Stable
+class CrowdZeroTypography internal constructor(
+
 )
