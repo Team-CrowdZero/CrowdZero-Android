39b0fc6803dd5ee3e2e5650daf72d9938e0f7c54
jjwm10625 <choyeongseo950@gmail.com>
2025-02-04 23:11:48 +0900

    [FIX] 캘린더 뷰모델, 엔티티 수정

--- domain/src/main/java/com/gdg/domain/entity/ScheduleEntity.kt
+++ domain/src/main/java/com/gdg/domain/entity/ScheduleEntity.kt
@@ -1,10 +1,12 @@
 package com.gdg.domain.entity
 
+import java.time.LocalDate
+
 data class ScheduleEntity(
+    val date: LocalDate,  // 집회정보가 등록된 날짜 필드
     val duration: String,
     val location: String,
     val region: String,
     val people: String,
     val jurisdiction: String
 )
-
--- feature/src/main/java/com/gdg/feature/calendar/CalendarViewModel.kt
+++ feature/src/main/java/com/gdg/feature/calendar/CalendarViewModel.kt
@@ -11,16 +11,43 @@ import javax.inject.Inject
 @HiltViewModel
 class CalendarViewModel @Inject constructor() : ViewModel() {
 
-    private val _mockSchedule = MutableStateFlow(
-        ScheduleEntity(
-            duration = "07:30 ~ 24:00",
-            location = "두터교회 앞 인도 및 2개 차로",
-            region = "한남동",
-            people = "3000",
-            jurisdiction = "용산"
+    private val _scheduleList = MutableStateFlow(
+        listOf(
+            ScheduleEntity(
+                date = LocalDate.of(2025, 2, 10), // 날짜 추가
+                duration = "07:30 ~ 24:00",
+                location = "두터교회 앞 인도 및 2개 차로",
+                region = "한남동",
+                people = "3000",
+                jurisdiction = "용산"
+            ),
+            ScheduleEntity(
+                date = LocalDate.of(2025, 2, 10), // 같은 날짜 다른 일정
+                duration = "10:00 ~ 15:00",
+                location = "구로구청 앞",
+                region = "구로 5동",
+                people = "1000",
+                jurisdiction = "구로"
+            ),
+            ScheduleEntity(
+                date = LocalDate.of(2025, 2, 10), // 같은 날짜 다른 일정
+                duration = "10:00 ~ 15:00",
+                location = "송현공원 앞 인도",
+                region = "송현동",
+                people = "500",
+                jurisdiction = "종로"
+            ),
+            ScheduleEntity(
+                date = LocalDate.of(2025, 2, 11), // 다른 날짜
+                duration = "14:00 ~ 18:00",
+                location = "청담고 앞",
+                region = "반포동",
+                people = "2000",
+                jurisdiction = "서초"
+            )
         )
     )
-    val mockSchedule: StateFlow<ScheduleEntity> = _mockSchedule
+    val scheduleList: StateFlow<List<ScheduleEntity>> = _scheduleList
 
     private val _selectedDate = MutableStateFlow(LocalDate.now())
     val selectedDate: StateFlow<LocalDate> = _selectedDate
