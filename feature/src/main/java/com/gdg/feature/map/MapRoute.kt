6b55aca1598a50162ef32e673d24df7170bea0fc
gaeulzzang <gaeulzzang@gmail.com>
2025-02-09 19:51:20 +0900

    #11 [UI] 모달 바텀 시트 구현

--- feature/src/main/java/com/gdg/feature/map/MapRoute.kt
+++ feature/src/main/java/com/gdg/feature/map/MapRoute.kt
@@ -3,6 +3,7 @@ package com.gdg.feature.map
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -13,13 +14,17 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -42,6 +47,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.zIndex
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.gdg.core.designsystem.component.chip.MapChip
@@ -111,7 +117,7 @@ fun MapRoute(
     )
 }
 
-@OptIn(ExperimentalNaverMapApi::class)
+@OptIn(ExperimentalNaverMapApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun MapScreen(
     mapProperties: MapProperties = MapProperties(),
@@ -123,8 +129,11 @@ fun MapScreen(
     onButtonClick: (Int) -> Unit = { }
 ) {
     var selectedLocation by remember { mutableStateOf<LocationType?>(null) }
+    var selectedRoad by remember { mutableStateOf<RoadEntity?>(null) }
     val coroutineScope = rememberCoroutineScope()
     val listState = rememberLazyListState()
+    val sheetState = rememberModalBottomSheetState()
+    var showBottomSheet by remember { mutableStateOf(false) }
 
     BackHandler(
         enabled = selectedLocation != null,
@@ -158,7 +167,15 @@ fun MapScreen(
                         width = 20.dp,
                         height = 20.dp,
                         state = MarkerState(position = LatLng(road.acdntY, road.acdntX)),
-                        icon = OverlayImage.fromResource(R.drawable.ic_ban_marker)
+                        icon = OverlayImage.fromResource(R.drawable.ic_ban_marker),
+                        onClick = {
+                            selectedRoad = road
+                            showBottomSheet = true
+                            coroutineScope.launch {
+                                sheetState.show()
+                            }
+                            true
+                        }
                     )
                 }
             }
@@ -197,6 +214,17 @@ fun MapScreen(
                 modifier = Modifier.align(Alignment.BottomCenter),
                 onButtonClick = onButtonClick
             )
+        }
+    }
+
+    if (showBottomSheet) {
+        ModalBottomSheet(
+            sheetState = sheetState,
+            onDismissRequest = { showBottomSheet = false },
+            containerColor = CrowdZeroTheme.colors.white,
+            shape = RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp)
+        ) {
+            RoadInfoSheet(road = selectedRoad)
         }
     }
 }
@@ -301,6 +329,77 @@ fun PlaceInfoCard(
     }
 }
 
+@Composable
+fun RoadInfoSheet(road: RoadEntity?) {
+    if (road == null) return
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 16.dp, bottom = 21.dp, end = 16.dp, top = 14.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .border(
+                    width = 0.5.dp,
+                    color = CrowdZeroTheme.colors.gray500,
+                    shape = RoundedCornerShape(10.dp)
+                )
+                .padding(horizontal = 13.dp, vertical = 12.dp),
+            verticalAlignment = Alignment.Top
+        ) {
+            Column {
+                Text(
+                    text = stringResource(R.string.map_modal_header),
+                    color = CrowdZeroTheme.colors.gray900,
+                    style = CrowdZeroTheme.typography.h5Bold
+                )
+                Text(
+                    text = stringResource(R.string.map_modal_generated_time, road.acdntOccrDt),
+                    color = CrowdZeroTheme.colors.gray700,
+                    style = CrowdZeroTheme.typography.c3Medium
+                )
+                Text(
+                    text = stringResource(R.string.map_modal_complete_time, road.expClrDt),
+                    color = CrowdZeroTheme.colors.gray700,
+                    style = CrowdZeroTheme.typography.c3Medium
+                )
+            }
+            Spacer(modifier = Modifier.weight(1f))
+            Image(
+                modifier = Modifier.size(35.dp),
+                imageVector = ImageVector.vectorResource(id = R.drawable.ic_ban_marker),
+                contentDescription = null
+            )
+        }
+        Column(
+            modifier = Modifier.padding(start = 5.dp)
+        ) {
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 18.dp, bottom = 3.dp),
+                text = stringResource(R.string.map_modal_description, road.acdntInfo),
+                color = CrowdZeroTheme.colors.gray800,
+                style = CrowdZeroTheme.typography.c3Medium,
+                textAlign = TextAlign.Start,
+                lineHeight = 13.sp
+            )
+            Text(
+                text = stringResource(R.string.map_modal_update_time, road.acdntTime),
+                color = CrowdZeroTheme.colors.gray700,
+                style = CrowdZeroTheme.typography.c4Regular
+            )
+            Text(
+                text = stringResource(R.string.map_modal_source),
+                color = CrowdZeroTheme.colors.gray700,
+                style = CrowdZeroTheme.typography.c4Regular,
+            )
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun PlaceInfoCardPreview() {
@@ -314,6 +413,24 @@ fun PlaceInfoCardPreview() {
                 max = 200
             ),
             onButtonClick = { }
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun RoadInfoSheetPreview() {
+    CrowdZeroAndroidTheme {
+        RoadInfoSheet(
+            road = RoadEntity(
+                areaId = 1,
+                acdntY = 37.123456,
+                acdntX = 127.123456,
+                acdntInfo = "세종대로 (교보빌딩 → 광화문역2번출구) 집회 무대설치로 진행방향 전차로 통제(반대가변운영)",
+                acdntOccrDt = "2021-09-01 12:00",
+                expClrDt = "2021-09-01 13:00",
+                acdntTime = "2021-09-01 12:00"
+            )
         )
     }
 }
