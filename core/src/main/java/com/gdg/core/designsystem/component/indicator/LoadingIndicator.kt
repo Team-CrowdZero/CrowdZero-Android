2c14e3bed6702527eafd3b348801e83254c84a05
gaeulzzang <gaeulzzang@gmail.com>
2025-02-14 22:39:23 +0900

    #13 [UI] loading indicator 구현

---
+++ core/src/main/java/com/gdg/core/designsystem/component/indicator/LoadingIndicator.kt
@@ -0,0 +1,31 @@
+package com.gdg.core.designsystem.component.indicator
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.gdg.core.designsystem.theme.CrowdZeroAndroidTheme
+import com.gdg.core.designsystem.theme.CrowdZeroTheme
+
+@Composable
+fun LoadingIndicator() {
+    Box(modifier = Modifier.fillMaxSize()) {
+        CircularProgressIndicator(
+            modifier = Modifier
+                .align(Alignment.Center),
+            color = CrowdZeroTheme.colors.green700,
+            trackColor = CrowdZeroTheme.colors.gray500
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoadingIndicatorPreview() {
+    CrowdZeroAndroidTheme {
+        LoadingIndicator()
+    }
+}
